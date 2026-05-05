2bb2b013abead2e36ed8e4717cb99349c0c575c5
zeliotmarketing <marketing@zeliot.in>
2026-05-05 12:05:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { PricingClient, GetProductsCommand } = require("@aws-sdk/client-pricing")
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors()); 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
